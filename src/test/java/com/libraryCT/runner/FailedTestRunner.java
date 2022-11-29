77d53731f9b01c4aab7167407ef26e360e090107
Tatsiana <tatsianavv@gmail.com>
2022-11-29 16:12:25 -0500

    All setups done, classes added:BasePage, DashboardPage, BookPage, LoginPage, UsersPage, utility classes, .properties file, .gitIgnore file

---
+++ src/test/java/com/libraryCT/runner/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.libraryCT.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/libraryCT/steps"
+
+)
+public class FailedTestRunner {
+}
